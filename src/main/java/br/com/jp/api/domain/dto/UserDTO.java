645bda1baf2a9d067bfb88db6b80f56da9f9931c
Jonathan Crespo <sicrano3000@gmail.com>
2022-03-28 16:55:47 -0300

    User DTO

---
+++ src/main/java/br/com/jp/api/domain/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package br.com.jp.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+}
