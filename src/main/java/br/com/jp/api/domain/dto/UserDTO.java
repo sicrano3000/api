3ca16cf43c01100052301aa464c9eef2ada0bd5b
Jonathan Crespo <sicrano3000@gmail.com>
2022-03-31 16:12:45 -0300

    removendo parametros não utilizados

--- src/main/java/br/com/jp/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/jp/api/domain/dto/UserDTO.java
@@ -3,13 +3,11 @@ package br.com.jp.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
