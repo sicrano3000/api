19fda71e1fbdd81d2101c8102fdeefb65c8f6f10
Jonathan Crespo <sicrano3000@gmail.com>
2022-04-02 17:01:26 -0300

    implementando logs no sistema

--- src/main/java/br/com/jp/api/resources/UserResource.java
+++ src/main/java/br/com/jp/api/resources/UserResource.java
@@ -3,6 +3,8 @@ package br.com.jp.api.resources;
 import br.com.jp.api.domain.dto.UserDTO;
 import br.com.jp.api.services.UserService;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,8 @@ import java.util.stream.Collectors;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    private static final Logger log = LoggerFactory.getLogger(UserResource.class);
+
     public static final String ID = "/{id}";
 
     @Autowired
@@ -26,11 +30,13 @@ public class UserResource {
 
     @GetMapping(value = ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        log.info("Buscando usuário por ID: " + id);
         return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
     }
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
+        log.info("Buscando todos os usuários");
         return ResponseEntity.ok()
                 .body(userService.findAll().stream()
                         .map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
@@ -38,6 +44,7 @@ public class UserResource {
 
     @PostMapping
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO) {
+        log.info("Criando um novo usuário");
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path(ID).buildAndExpand(userService.create(userDTO).getId()).toUri();
         return ResponseEntity.created(uri).build();
@@ -45,12 +52,14 @@ public class UserResource {
 
     @PutMapping(value = ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
+        log.info("Atualizando usuário de ID: " + id);
         userDTO.setId(id);
         return ResponseEntity.ok().body(mapper.map(userService.update(userDTO), UserDTO.class));
     }
 
     @DeleteMapping(value = ID)
     public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
+        log.info("Deletando usuário de ID: " + id);
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
